3a8e1b7d129b6d42bc3e733a3bc1a7894f2fba14
Troels Liebe Bentsen <tlb@rapanden.dk>
2025-09-16 10:57:02 +0200

    Fix not showing logo in mobile mode.

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -32,7 +32,7 @@ export function Navbar() {
 			<div className="container flex h-16 items-center justify-between">
 				<div className="flex items-center">
 					<a href="/" className="flex items-center space-x-2">
-						<span className="hidden text-xl font-bold sm:inline-block">
+						<span className="text-xl font-bold sm:inline-block">
 							<img
 								src="/bestcloudresell-logo-plain.svg"
 								alt="Logo"
